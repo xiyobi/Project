e2e32298e8da025280ef201a84182cdaaacb3ebe
xiyobidasturchi0011@mail.com <xiyobidasturchi0011@gmail.com>
2024-11-02 19:06:42 +0500

    Small project CSS and HTML:

---
+++ one.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shehroz</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <div id="header">
+        <div class="container">
+            <h1>Website</h1>
+        </div>
+    </div>
+    <div id="navbar">
+        <div class="container">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">CONTACS</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="showcase">
+        <div class="container">
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Odit esse aliquid quos nam maxime sequi possimus
+                unde eaque fuga libero?
+            </h1>
+        </div>
+    </div>
+    <div id="main">
+        <div class="container">
+            <div class="content">
+                <h3>Welcom</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+                    nulla ea id impedit totam commodirepellendus veniam dicta nesciunt ducimus earum,
+                    vel molestias nisi voluptatum tenetur blanditiis nostrum incidunt voluptas?
+                    Iure facilis labore accusantium nemo repellendus veniam,
+                    error minima praesentium.</p>
+            </div>
+            <div class="sidebar">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, inventore.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class = "clr">
+        
+    </div>
+    <div id = "footer">
+        <p>GITHUB user Xiyobi</p>
+    </div>
+</body>
+
+</html>
